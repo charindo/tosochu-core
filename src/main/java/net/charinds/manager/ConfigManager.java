6a5a936366fe8322c17ae53d657f5f481cb3ac99
charindo <charindou@gmail.com>
2021-10-16 16:07:51 +0900

    Configの取得方法を変更

--- src/main/java/net/charinds/manager/ConfigManager.java
+++ src/main/java/net/charinds/manager/ConfigManager.java
@@ -6,12 +6,22 @@ import net.charinds.store.CustomConfig;
 public class ConfigManager{
 
     public static CustomConfig config;
-    public static CustomConfig admins;
+    public static CustomConfig admin;
 
     public ConfigManager(){
         config = new CustomConfig(Core.core);
         config.saveDefaultConfig();
-        admins = new CustomConfig(Core.core, "admin.yml");
-        admins.saveDefaultConfig();
+        admin = new CustomConfig(Core.core, "admin.yml");
+        admin.saveDefaultConfig();
+    }
+
+    public static CustomConfig getCustomConfig(String configName) {
+        if (configName.equalsIgnoreCase("config")) {
+            return config;
+        } else if (configName.equalsIgnoreCase("admin")) {
+            return admin;
+        } else {
+            throw new IllegalArgumentException();
+        }
     }
 }
