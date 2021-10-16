b8a8cf89b82f412dcdc50f260ab760fe9834c9d5
charindo <charindou@gmail.com>
2021-10-16 16:06:54 +0900

    タイマータスク初期化

--- src/main/java/net/charinds/scheduler/Timer.java
+++ src/main/java/net/charinds/scheduler/Timer.java
@@ -7,16 +7,7 @@ public class Timer extends BukkitRunnable {
 
     private Integer i;
 
-    public Timer(Integer i) {
-        this.i = i;
-    }
-
     @Override
     public void run() {
-        i -= 1;
-        Bukkit.getServer().broadcastMessage(i.toString());
-        if (i <= 0) {
-            cancel();
-        }
     }
 }
