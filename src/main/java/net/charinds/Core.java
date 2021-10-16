a63425ef22a9b632542ce60de4028d53249d009f
charindo <charindou@gmail.com>
2021-10-16 15:02:09 +0900

    Update Core.java

--- src/main/java/net/charinds/Core.java
+++ src/main/java/net/charinds/Core.java
@@ -9,6 +9,8 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Core extends JavaPlugin implements Listener {
 
+    public static String VERSION = "1.0.0";
+
     public static Core core;
 
     @Override
